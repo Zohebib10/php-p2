9dc93f8a4f66570962e60702953c5f237514011a
Zohebib10 <9023992@student.zadkine.nl>
2023-12-14 08:56:46 +0100

    1

---
+++ project2_menu/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Voorbeeld2 menu project</title>
+</head>
+<body>
+    <h1>Voorbeeld2 menu project</h1>
+
+  <nav>
+<ul>
+     <li><a href="index.php">Home</a>
+     <li><a href="page1.php">Page 2</a>
+     li><a href="page1.php">Page 2</a>
+  </li>
+</ul>
+  </nav>
+</body>
+</html>
